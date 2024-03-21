637fa1afee333f979b8bf267e3c4987fb2dead5f
Tre-brock <ebrocklesby77@gmail.com>
2024-03-21 16:58:47 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-    alert("Hello, World!");
+<script>
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+var myString = "Hello World!";
+alert(myString);
+// @license-end
+</script>
+
+
+alert("Hello, World!");
 
 console.log('It works!');
 
