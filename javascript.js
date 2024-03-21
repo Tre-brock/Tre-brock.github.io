c1cc5d15a27433e76ed5320fe362b17fcc9c4857
Tre-brock <ebrocklesby77@gmail.com>
2024-03-21 17:05:06 -0400

    testing javascript

--- javascript.js
+++ javascript.js
@@ -1,12 +1,5 @@
-<script>
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-var myString = "Hello World!";
+var myString = "test!";
 alert(myString);
 // @license-end
-</script>
-
-
-alert("Hello, World!");
-
-console.log('It works!');
 
